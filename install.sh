e1dc8ab72b3cdcd067e5c0279b09b0d468a70441
Saman <ebsa491@gmail.com>
2020-05-22 17:51:02 +0430

    Program completed

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Lesser General Public License (LGPL) v3.0 .
+# The project has been developed by Saman Ebrahimnezhad .
+# Created at Iran (IR) .
+
+sudo mkdir /etc/Notebook/
+sudo mkdir /etc/Notebook/notes
+
+sudo cp bin/Notebook /bin/
+sudo cp icon.txt /etc/Notebook
+
+echo "Installed..."
--- src/Notebook.py
+++ src/Notebook.py
@@ -5,11 +5,38 @@
 # Python 3 .
 
 import sys
+import os
 
 class Notebook:
 
     def __init__(self):
-        
+
+        iconFile = open("/etc/Notebook/icon.txt", 'r')
+
+        # Figlet used
+
+        icon = iconFile.read()
+
+        print(icon)
+
+        if(len(sys.argv) > 1):
+
+            self.filename = self.replacer(sys.argv[1])
+
+        else:
+
+            os.system("ls /etc/Notebook/notes/")
+
+            self.filename = input("Enter the text name: ")
+
+            self.filename = self.replacer(self.filename)
+
+        self.text = input("Enter the text: ")
+
+        os.system("echo \"" + self.replacer(self.text) + "\" > /etc/Notebook/notes/" + self.filename + ".txt")
+
+    def replacer(self, string):
+        return str(string).replace(" *:!@#$%^&|\'\";><?~.,", "")
 
 def main():
     app = Notebook()
